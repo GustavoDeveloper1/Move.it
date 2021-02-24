da9631a547f8d3aea5fc86e2e9ea4fb9fe9f4e4a
GustavoDeveloper1 <devinvestidor02@gmail.com>
2021-02-24 20:36:28 -0300

    fazendo a parte perfil

---
+++ moveit-next/src/components/Perfil.tsx
@@ -0,0 +1,16 @@
+
+
+export function Profile() {
+    return (
+        <div className="profile-container">
+            <img src="https://github.com/GustavoDeveloper1.png" alt="Gustavo Dias" />
+            <div>
+                <strong>Gustavo Dias</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,12 +1,23 @@
 import ExperienceB from '../components/ExperienceBar';
 import Head from 'next/head';
+import { Profile } from '../components/Perfil';
 
 
 export default function Home() {
   return (
     <div className="container">
-          <ExperienceB />
-  </div>
-    
+      <ExperienceB />
+
+      <section>
+        <div>
+          <Profile/>
+        </div>
+        
+        <div>
+
+        </div>
+      </section>
+    </div>
+
   )
 }
