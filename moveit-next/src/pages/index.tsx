a816a2454faf549fb38de804153bcb9c80b9e2ea
GustavoDeveloper1 <devinvestidor02@gmail.com>
2021-02-24 21:45:33 -0300

    fazendo a parte de count down

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,17 +2,22 @@ import ExperienceB from '../components/ExperienceBar';
 import Head from 'next/head';
 import { Profile } from '../components/Perfil';
 import { CompleteChalenges } from '../components/CompleteChalenge';
+import { CountDown } from '../components/CountDown';
 
 
 export default function Home() {
   return (
     <div className="container">
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceB />
 
       <section>
         <div>
           <Profile/>
           <CompleteChalenges/>
+          <CountDown/>
         </div>
         
         <div>
